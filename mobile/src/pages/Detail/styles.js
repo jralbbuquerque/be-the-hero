b0810c5e12d7cf98ef6eedaa29074b3daded0125
Francisco Júnior <jralbbuquerque@gmail.com>
2020-04-04 15:29:12 -0300

    Configuração do app mobile com a API

--- mobile/src/pages/Detail/styles.js
+++ mobile/src/pages/Detail/styles.js
@@ -26,7 +26,7 @@ export default StyleSheet.create({
         fontSize: 14,
         color: '#41414d',
         fontWeight: 'bold',
-        marginBottom: 24,
+        marginTop: 24,
     },
 
     incidentValue: {
